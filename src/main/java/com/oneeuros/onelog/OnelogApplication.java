50574fdbe9ca64978c7207da0f6f155a0e551c68
Admin <wlsql852@naver.com>
2026-05-01 17:33:51 +0900

    initial commit

---
+++ src/main/java/com/oneeuros/onelog/OnelogApplication.java
@@ -0,0 +1,13 @@
+package com.oneeuros.onelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnelogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oneeuros/onelog/OnelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneeuros.onelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
