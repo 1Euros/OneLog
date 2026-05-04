e26ed3db50c3139f68f679ea0d7f7ef86d9ead57
Admin <wlsql852@naver.com>
2026-05-04 22:24:49 +0900

    [Add] 유효성검사 댓글 작성 @Valid로 이동 변경 - 미완성

--- src/main/java/com/oneeuros/onelog/comment/CommentService.java
+++ src/main/java/com/oneeuros/onelog/comment/CommentService.java
@@ -18,14 +18,14 @@ public class CommentService {
         String nickname = requestDto.nickname();
         String content = requestDto.content();
         String password = requestDto.password();
-        // 유효성 검증
-        // 닉네임 검증
-        if(nickname == null || nickname.trim().isEmpty()) throw new IllegalArgumentException("닉네임은 빈칸이 불가능합니다.");
-        else if (nickname.length() > 30) throw new IllegalArgumentException("닉네임은 30자 이하만 가능합니다");
-        // 내용 빈칸 검증
-        if(content == null || content.trim().isEmpty()) throw new IllegalArgumentException("내용은 빈칸이 불가능합니다.");
-        // 비밀번호 6자리 검증
-        PasswordUtils.validatePassword(password);
+//        // 유효성 검증
+//        // 닉네임 검증
+//        if(nickname == null || nickname.trim().isEmpty()) throw new IllegalArgumentException("닉네임은 빈칸이 불가능합니다.");
+//        else if (nickname.length() > 30) throw new IllegalArgumentException("닉네임은 30자 이하만 가능합니다");
+//        // 내용 빈칸 검증
+//        if(content == null || content.trim().isEmpty()) throw new IllegalArgumentException("내용은 빈칸이 불가능합니다.");
+//        // 비밀번호 6자리 검증
+//        PasswordUtils.validatePassword(password);
 
         // 비밀번호 인코딩
         String encodedPassword = PasswordUtils.encodePassword(password);
--- src/main/java/com/oneeuros/onelog/comment/dto/CommentCreateRequestDto.java
+++ src/main/java/com/oneeuros/onelog/comment/dto/CommentCreateRequestDto.java
@@ -1,8 +1,18 @@
 package com.oneeuros.onelog.comment.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+@Valid
 public record CommentCreateRequestDto(
+        @NotBlank(message = "닉네임을 입력해주세요.")
+        @Size(max = 30, message = "닉네임은 30자 이하입니다.")
         String nickname,
+        @Pattern(regexp = "^[0-9]{6}$")
         String password,
+        @NotBlank(message = "내용을 입력해주세요.")
         String content
 ) {
 }
