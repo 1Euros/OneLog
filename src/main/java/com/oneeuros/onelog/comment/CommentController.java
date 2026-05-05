30273d69658c6393a6d5ef6896e5a0f812fb1636
wlsql852@naver.com <wlsql852@naver.com>
2026-05-05 17:32:22 +0900

    [Add] 유효성검사 댓글 작성 @Valid로 이동 변경 - 완성

--- src/main/java/com/oneeuros/onelog/comment/CommentController.java
+++ src/main/java/com/oneeuros/onelog/comment/CommentController.java
@@ -25,21 +25,24 @@ public class CommentController {
     @PostMapping("/create")
     public String saveComment(@Valid CommentCreateRequestDto requestDto, BindingResult bindingResult,  Model model) {
         // 유효성 검증
-        if (bindingResult.hasFieldErrors("nickname"))  {
-            var errors = bindingResult.getFieldErrors("nickname");
-            for (var error:errors) {
-                String code = error.getCode();
-                if ("NotBlank".equals(code))
-                    model.addAttribute("errorMessage", "닉네임을 입력해주세요.");
-                else if ("Size".equals(code))
-                    model.addAttribute("errorMessage", "닉네임은 30자 이하입니다.");
+        if (bindingResult.hasErrors()) {
+            if (bindingResult.hasFieldErrors("nickname")) {
+                var errors = bindingResult.getFieldErrors("nickname");
+                for (var error : errors) {
+                    String code = error.getCode();
+                    if ("NotBlank".equals(code))
+                        model.addAttribute("errorMessage", "닉네임을 입력해주세요.");
+                    else if ("Size".equals(code))
+                        model.addAttribute("errorMessage", "닉네임은 30자 이하입니다.");
+                }
             }
-        }
-        if (bindingResult.hasFieldErrors("password"))
-            model.addAttribute("errorMessage", "비밀번호는 숫자 6자리여야 합니다.");
-        if (bindingResult.hasFieldErrors("content"))
-            model.addAttribute("errorMessage", "내용을 입력해주세요.");
+            if (bindingResult.hasFieldErrors("password"))
+                model.addAttribute("errorMessage", "비밀번호는 숫자 6자리여야 합니다.");
+            if (bindingResult.hasFieldErrors("content"))
+                model.addAttribute("errorMessage", "내용을 입력해주세요.");
 
+            return "comments/showInputComment";
+        }
         Comment comment = commentService.save(requestDto);
         model.addAttribute("comment", comment);
         return "comments/comments";
--- src/main/java/com/oneeuros/onelog/comment/CommentService.java
+++ src/main/java/com/oneeuros/onelog/comment/CommentService.java
@@ -15,23 +15,11 @@ public class CommentService {
     // 댓글 저장
     @Transactional
     public Comment save(CommentCreateRequestDto requestDto) {
-        String nickname = requestDto.nickname();
-        String content = requestDto.content();
-        String password = requestDto.password();
-//        // 유효성 검증
-//        // 닉네임 검증
-//        if(nickname == null || nickname.trim().isEmpty()) throw new IllegalArgumentException("닉네임은 빈칸이 불가능합니다.");
-//        else if (nickname.length() > 30) throw new IllegalArgumentException("닉네임은 30자 이하만 가능합니다");
-//        // 내용 빈칸 검증
-//        if(content == null || content.trim().isEmpty()) throw new IllegalArgumentException("내용은 빈칸이 불가능합니다.");
-//        // 비밀번호 6자리 검증
-//        PasswordUtils.validatePassword(password);
-
         // 비밀번호 인코딩
-        String encodedPassword = PasswordUtils.encodePassword(password);
+        String encodedPassword = PasswordUtils.encodePassword(requestDto.password());
 
         // 데이터 저장
-        Comment comment = new Comment(nickname, encodedPassword, content);
+        Comment comment = new Comment(requestDto.nickname(), encodedPassword, requestDto.content());
         return commentRepository.save(comment);
     }
 }
