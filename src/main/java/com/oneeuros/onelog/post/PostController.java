9ccb538cdff9d59cea3484b9519d622818752e25
coco31821 <coco318213@gmail.com>
2026-05-05 15:41:39 +0900

    [Feat] RequestDTO 추가 및 닉네임 필수값 검증

--- src/main/java/com/oneeuros/onelog/post/PostController.java
+++ src/main/java/com/oneeuros/onelog/post/PostController.java
@@ -15,21 +15,15 @@ public class PostController {
     private final PostService postService;
 
 
-
-
     // 글 작성 처리
     @PostMapping("/create")
     public String savePost(
-            @RequestParam String title,
-            @RequestParam String content,
-            @RequestParam String nickname,
-            @RequestParam String password,
-//            @RequestParam Long boardId, // 추후 board 받을 때 추가
+            PostCreateRequest request,
             Model model
     ){
-        Post post = postService.save(title, content, nickname, password);
+        Post post = postService.save(request);
         model.addAttribute("post",post);
-        return "posts/detail";  // 생성 완료 후 작성된 글로 넘겨야하지만, test를 위해 detail.html 추가
+        return "posts/detail";  // 생성 완료 후 작성된 글로 넘겨야하지만, test를 위해 posts/detail.html 추가
     }
 
 
--- src/main/java/com/oneeuros/onelog/post/PostService.java
+++ src/main/java/com/oneeuros/onelog/post/PostService.java
@@ -11,10 +11,20 @@ public class PostService {
     private final PostRepository postRepository;
 
     // post로 받는 save()
-    public Post save(String title, String content, String nickname, String password){
-        PasswordValidator.validatePassword(password);
-        Post post = new Post(title, content, nickname, password);
+    public Post save(PostCreateRequest request) {
+        // 닉네임 필수값 검증
+        validateNickname(request.nickname());
+
+        PasswordValidator.validatePassword(request.password());
+        Post post = new Post(request.title(), request.content(), request.nickname(), request.password());
         return postRepository.save(post);
+    }
+
+
+    private void validateNickname(String nickname) {
+        if (nickname == null || nickname.trim().isEmpty()) {
+            throw new IllegalArgumentException("닉네임은 필수입니다.");
+        }
     }
 
 
@@ -25,4 +35,5 @@ public class PostService {
         Post post = new Post(title, content, nickname, password);
         return postRepository.save(post);
     }*/
+
 }
