6f2ff9974c56f1c03d3d41ec2b1cbed2aa740d70
nodh020403 <nodh020403n@naver.com>
2026-05-05 15:43:16 +0900

    [Feat] get 방식으로 게시판 생성 기능 구현(중복 방지, trim 기능)

--- src/main/java/com/oneeuros/onelog/board/BoardController.java
+++ src/main/java/com/oneeuros/onelog/board/BoardController.java
@@ -3,9 +3,9 @@ package com.oneeuros.onelog.board;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
     private final BoardService boardService;
 
-    @GetMapping ("/create")
-    public String saveBoard(String name, Model model) {
+    @PostMapping ("/create")
+    public String saveBoard(
+            @RequestParam String name,
+            Model model) {
         if (name == null || name.trim().isEmpty()) {
             throw new RuntimeException("게시판 이름은 필수입니다");
         }
@@ -22,5 +24,16 @@ public class BoardController {
         model.addAttribute("board", board);
         return "boards/boards";
     }
+
+    // Get 방식
+//    @GetMapping ("/create")
+//    public String saveBoard(String name, Model model) {
+//        if (name == null || name.trim().isEmpty()) {
+//            throw new RuntimeException("게시판 이름은 필수입니다");
+//        }
+//        Board board = boardService.save(name.trim());
+//        model.addAttribute("board", board);
+//        return "boards/boards";
+//    }
 
 }
