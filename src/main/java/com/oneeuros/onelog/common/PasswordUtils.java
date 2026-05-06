ef6fbd2e11d578b3189de3f8b61d4c78e77664ed
Admin <wlsql852@naver.com>
2026-05-06 20:30:27 +0900

    [Add] PasswordUtils에서 모든 메서드가 static이므로 @Component 해제, 필요없는 로직 제거

--- src/main/java/com/oneeuros/onelog/common/PasswordUtils.java
+++ src/main/java/com/oneeuros/onelog/common/PasswordUtils.java
@@ -1,19 +1,8 @@
 package com.oneeuros.onelog.common;
 
-import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
 public class PasswordUtils {
-
-    //숫자 6자리 검증
-    public static void validatePassword(String password) {
-        if (password == null || !password.matches("^[0-9]{6}$")) {
-            throw new IllegalArgumentException("비밀번호는 숫자 6자리여야 합니다.");
-        }
-    }
 
     //패스워드 인코딩
     public static String encodePassword(String password) {
