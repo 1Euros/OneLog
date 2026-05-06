5adaec075493c44e2915d123208684b34f6a7137
Admin <wlsql852@naver.com>
2026-05-06 15:13:33 +0900

    [Mod] 댓글 서비스의 게시글 의존성 변경

--- src/main/java/com/oneeuros/onelog/comment/CommentService.java
+++ src/main/java/com/oneeuros/onelog/comment/CommentService.java
@@ -3,7 +3,6 @@ package com.oneeuros.onelog.comment;
 import com.oneeuros.onelog.comment.dto.CommentCreateRequestDto;
 import com.oneeuros.onelog.common.PasswordUtils;
 import com.oneeuros.onelog.post.Post;
-import com.oneeuros.onelog.post.PostRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,15 +13,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
-    private final PostRepository postRepository;
 
 
     // 댓글 저장
     @Transactional
-    public void save(Long postId, CommentCreateRequestDto requestDto) {
-        //유효성 검사
-        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 아이디의 게시글이 없습니다."));
-
+    public void save(Post post, CommentCreateRequestDto requestDto) {
         // 비밀번호 인코딩
         String encodedPassword = PasswordUtils.encodePassword(requestDto.password());
 
@@ -33,9 +28,6 @@ public class CommentService {
 
     // 댓글 목록 조회
     public List<Comment> findComments(Long postId) {
-        // 해당 게시글 있는지 확인
-        postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("해당 아이디의 게시글이 없습니다."));
-
         return commentRepository.findAllByPostIdOrderByCreatedAtDesc(postId);
 
     }
