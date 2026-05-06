3c11761b361182df3faf1cd8a26c862f214bd4eb
Admin <wlsql852@naver.com>
2026-05-06 20:56:35 +0900

    [Mod] 댓글 관련 조회 메서드에 @Transactional(readOnly=true) 사용

--- src/main/java/com/oneeuros/onelog/comment/CommentService.java
+++ src/main/java/com/oneeuros/onelog/comment/CommentService.java
@@ -4,9 +4,10 @@ import com.oneeuros.onelog.comment.dto.CommentCreateRequestDto;
 import com.oneeuros.onelog.comment.dto.CommentUpdateRequestDto;
 import com.oneeuros.onelog.common.PasswordUtils;
 import com.oneeuros.onelog.post.Post;
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class CommentService {
     }
 
     // 댓글 목록 조회
+    @Transactional(readOnly = true)
     public List<Comment> findComments(Long postId) {
         return commentRepository.findAllByPostIdOrderByCreatedAtDesc(postId);
 
@@ -49,16 +51,19 @@ public class CommentService {
     }
 
     // 해당 게시글의 최신순으로 첫번째 댓글 조회
+    @Transactional(readOnly = true)
     public Comment findFirstComment(Long postId) {
         return commentRepository.findFirstByPostIdOrderByCreatedAtDesc(postId).orElse(null);
     }
 
     //해당 게시글의 댓글 수 조회
+    @Transactional(readOnly = true)
     public int countCommentsByPostId(Long postId) {
         return commentRepository.countByPostId(postId);
     }
 
     //수정/삭제시 비밀번호 확인
+    @Transactional(readOnly = true)
     public Boolean confirmPassword(Long commentId, String password) {
         //댓글 존재 확인
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("해당하는 댓글이 존재하지 않습니다."));
